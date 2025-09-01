667a377cf387839e04c447aab053cc23a0972c5a
Sven Jens <sven@sven-jens.nl>
2025-09-01 19:46:20 +0200

    fix: remove invalid MongoDB client options
    - Remove bufferMaxEntries and bufferCommands (Mongoose-specific)
    - Keep only valid MongoDB driver connection options

--- src/utils/database.js
+++ src/utils/database.js
@@ -32,8 +32,6 @@ export async function connectToDatabase() {
       maxPoolSize: 10, // Maintain up to 10 socket connections
       serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
       socketTimeoutMS: 45000, // Close connections after 45 seconds of inactivity
-      bufferMaxEntries: 0, // Disable mongoose buffering
-      bufferCommands: false, // Disable mongoose buffering
     });
 
     // Connect the client to the server
